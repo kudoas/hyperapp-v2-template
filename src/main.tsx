a5a8650447591446bf6ff3bd9abf42de27229cc8
Daichi Kudo <sakanactor624@gmail.com>
2021-08-17 14:53:32 +0900

    Add starter component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,39 @@
 import { app } from "hyperapp";
 import h from "hyperapp-jsx-pragma";
 
-// @jsx h
+type State = {
+  todos: string[];
+  value: string;
+};
+
+const state = { todos: [], value: "" };
+
+const NewValue = (state: State, event: any) => {
+  state.value = event.target.value;
+  return {
+    ...state,
+    value: event.target.value,
+  };
+};
+
+const AddTodo = (state: State) => ({
+  ...state,
+  value: "",
+  todos: state.todos.concat(state.value),
+});
 
 app({
-  init: { name: "Fuji Souta" },
-  view: (state) => (
-    <main>
+  init: state,
+  view: ({ todos, value }) => (
+    <main style={{ color: "green" }}>
       <h1>Hello Hyperapp</h1>
-      <p>{state.name}</p>
+      <input type="text" onchange={NewValue} value={value} />
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+      <button onclick={AddTodo}>New!</button>
     </main>
   ),
   node: document.getElementById("app")!,
